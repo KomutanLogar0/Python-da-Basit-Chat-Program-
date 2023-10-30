94052c07a97e2bcd525dce022f507f23fd392d7a
KomutanLogar0 <115404688+KomutanLogar0@users.noreply.github.com>
2023-10-30 09:37:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import socket
+
+IP = 192.168.1.38   
+PORT = 8080
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((IP,PORT))
+s.listen(5)
+print("Sunucu kuruldu. İstemci/İstemciler bekleniliyor.")
+
+try:
+    s.accept((istemci, adres))
+    baglanti = True
+    print("İstemci/İstemciler sunucuya bağlandı!")
+
+except Exception as hata:
+    print(f"Hata: {hata}")
